64b7050f90a29bf552563b07bd4934760bb00d89
crosalot <crosalot#gmail.com>
2015-03-21 12:11:38 +0700

    move settings

--- settings.js
+++ settings.js
@@ -2,5 +2,5 @@ FACEBOOK_APP_ID = '445472018940505';
 FACEBOOK_EXTENDED_PERMISSIONS = 'email,user_friends';
 
 
-LOGIN_TEMPLATE = '/static/app/account/login/login.html';
-SIGNUP_TEMPLATE = '/static/app/account/signup/signup.html';
+LOGIN_TEMPLATE = '/static/app/odmbase/account/login/login.html';
+SIGNUP_TEMPLATE = '/static/app/odmbase/account/signup/signup.html';
